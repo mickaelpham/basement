9fc05e28ee2053e526d46eb2065fea938cdccdf1
Mickael Pham <mickael.pham@gmail.com>
2016-12-13 14:51:53 -0800

    Add gulp-changed and finalize Browsersync integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var autoprefixer = require('autoprefixer');
+var browserSync  = require('browser-sync').create();
+var changed      = require('gulp-changed');
 var cssnano      = require('cssnano');
 var gulp         = require('gulp');
 var precss       = require('precss');
@@ -7,7 +9,9 @@ var postcss      = require('gulp-postcss');
 // Copy HTML files from src/ to build/ directory
 gulp.task('html', function() {
   return gulp.src('src/*.html')
-    .pipe(gulp.dest('build'));
+    .pipe(changed('build', { extension: '.html' }))
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.stream());;
 });
 
 // Process CSS and copy to build/ directory
@@ -19,13 +23,26 @@ gulp.task('css', function() {
   ];
   return gulp.src('src/**/*.css')
     .pipe(postcss(processors))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.stream());
 });
 
 // Watch for file changes and process them accordingly
 gulp.task('watch', ['html', 'css'], function() {
   gulp.watch('src/*.html', ['html']);
   gulp.watch('src/**/*.css', ['css']);
+});
+
+// Serve the build/ directory with Browsersync and watch them
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+    notify: false
+  });
+
+  // gulp.watch('build/*.html').on('change', browserSync.reload)
 });
 
 gulp.task('default', function() {
