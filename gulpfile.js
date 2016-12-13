584af2f328fae92ca4127723a77e71874210aab1
Mickael Pham <mickael.pham@gmail.com>
2016-12-13 13:11:57 -0800

    Add HTML task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 
+// Copy HTML files from src/ to build/ directory
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('build'));
+});
+
 gulp.task('default', function() {
   console.log('Hello, world!');
 });
